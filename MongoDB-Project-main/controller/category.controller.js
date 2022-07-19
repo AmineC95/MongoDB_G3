cb257ce3b4f44b75c2acee0efeb9e29575b4c750
Christopher FRENOIS <cfrenois@mbcloud.fr>
2022-07-19 10:57:37 +0200

    category finish

--- MongoDB-Project-main/controller/category.controller.js
+++ MongoDB-Project-main/controller/category.controller.js
@@ -1,5 +1,7 @@
 //const Comment = require('../model/category.model');
 
+const Category = require("../model/category.model");
+
 
 /**
  * Récupère les categories
@@ -14,7 +16,12 @@ exports.getAll = (req, res, next) => {
  * @param {string} req.params.id id du categorie à récupérer
  */
 exports.get = (req, res, next) => {
-
+    let categorie = await Category.findOne({ _id: req.params.id });
+    if (categorie) {
+        res.status(200).json({ categorie });
+    } else {
+        res.status(404).json({ message: "Votre categorie n'est pas reconnu" });
+    }
 };
 
 /**
@@ -22,7 +29,11 @@ exports.get = (req, res, next) => {
  * @param {string} req.body informations de la categorie à créer
  */
 exports.create = (req, res) => {
-
+    let category = new Category();
+ category.title = req.body.name;
+ category.content = req.body.description;
+ category.save();
+ res.status(200).json({ category });
 };
 
 /**
@@ -30,5 +41,12 @@ exports.create = (req, res) => {
  * @param {string} req.params.id id de la categorie à supprimer
  */
 exports.delete = (req, res, next) => {
-
+    let category = await Category.findOne({ _id: req.params.id });
+    if (category) {
+        category.delete();
+        
+        res.status(200).json({ message: "Category supprimé" });
+    } else {
+        res.status(404).json({ message: "Category non trouvé" });
+    }
 };
